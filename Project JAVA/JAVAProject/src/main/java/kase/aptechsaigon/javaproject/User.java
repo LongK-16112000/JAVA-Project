82cdbf692dec4023eddea4f0c4cbc3fdbcfb6025
Windows <Windows@DESKTOP-V3M7KUT>
2025-01-09 20:02:19 +0700

    thêm dữ liệu vào file user.java

--- Project JAVA/JAVAProject/src/main/java/kase/aptechsaigon/javaproject/User.java
+++ Project JAVA/JAVAProject/src/main/java/kase/aptechsaigon/javaproject/User.java
@@ -12,12 +12,14 @@ package kase.aptechsaigon.javaproject;
     private String username;
     private String password;
     private String fullName;
+    private String address;
 
     // Constructor để khởi tạo đối tượng User
     public User(String username, String password, String fullName) {
         this.username = username;
         this.password = password;
         this.fullName = fullName;
+        this.address = address;
     }
 
     // Getter và Setter cho username
@@ -46,12 +48,22 @@ package kase.aptechsaigon.javaproject;
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
+    
+     // Getter và Setter cho addRess
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String fullName) {
+        this.address = address;
+    }
 
     // Override phương thức toString() để in thông tin User
     @Override
     public String toString() {
         System.out.println("---Thong Tin Nguoi Dung---");
-        return "username='" + username + "', password='" + password + "', fullName='" + fullName + "'";
+        return "username='" + username + "', password='" + password + "', fullName='" + fullName + "'"
+                + "address='" + address;
     }
 
 
